1c69efb576cdc9aab5ff78504a5a68aa872e7b4d
euics <pjh575157@gmail.com>
2024-07-14 23:06:59 +0900

    Initial Commit

---
+++ src/main/java/paratrip/paratrip/ParatripApplication.java
@@ -0,0 +1,13 @@
+package paratrip.paratrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParatripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParatripApplication.class, args);
+	}
+
+}
