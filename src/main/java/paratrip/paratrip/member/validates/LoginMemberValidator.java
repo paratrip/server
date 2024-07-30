da2740767204c4562f92c336a60a47f64ccbd89c
euics <pjh575157@gmail.com>
2024-07-30 17:53:03 +0900

    feat: 로그인 요청 유효성 검사기 추가 (#3)
    - 로그인 요청을 위한 LoginMemberValidator 클래스 추가
      - 이메일 및 비밀번호 유효성 검사 메서드 추가
      - 이메일 또는 비밀번호가 null이거나 비어있을 경우 BadRequestException을 발생시키도록 설정
      - LoginMemberRequest의 이메일 및 비밀번호 유효성 검사

---
+++ src/main/java/paratrip/paratrip/member/validates/LoginMemberValidator.java
@@ -0,0 +1,28 @@
+package paratrip.paratrip.member.validates;
+
+import static paratrip.paratrip.member.vo.request.MemberRequestVo.*;
+
+import org.springframework.stereotype.Component;
+
+import paratrip.paratrip.core.exception.BadRequestException;
+import paratrip.paratrip.core.exception.ErrorResult;
+
+@Component
+public class LoginMemberValidator {
+	public void validate(LoginMemberRequest request) {
+		validateEmail(request.email());
+		validatePassword(request.password());
+	}
+
+	private void validateEmail(String email) {
+		if (email == null || email.isEmpty()) {
+			throw new BadRequestException(ErrorResult.DTO_BAD_REQUEST_EXCEPTION);
+		}
+	}
+
+	private void validatePassword(String password) {
+		if (password == null || password.isEmpty()) {
+			throw new BadRequestException(ErrorResult.DTO_BAD_REQUEST_EXCEPTION);
+		}
+	}
+}
