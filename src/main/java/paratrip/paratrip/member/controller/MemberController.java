34fdd1260ac6a480d7a352436eaa89620e85339e
euics <pjh575157@gmail.com>
2024-07-30 17:06:27 +0900

    feat: 회원 가입 요청 API 추가 (#1)
    - 회원 가입 요청 API 엔드포인트 추가
    - Gender 필드를 MALE, FEMALE로 구분하여 처리
    - 이메일 및 사용자 ID 중복 처리 로직 포함
    - 성공 및 오류 응답 코드 및 메시지 정의
      - 200: 요청에 성공
      - S500: 서버 오류
      - B001: 요청 값 형식 오류
      - UIDC002: 사용자 ID 중복 오류
      - EDC001: 이메일 중복 오류
    - 요청 값 유효성 검사 및 회원 가입 서비스 호출 로직 추가

--- src/main/java/paratrip/paratrip/member/controller/MemberController.java
+++ src/main/java/paratrip/paratrip/member/controller/MemberController.java
@@ -19,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 import paratrip.paratrip.core.base.BaseResponse;
 import paratrip.paratrip.core.exception.GlobalExceptionHandler;
 import paratrip.paratrip.member.service.MemberService;
+import paratrip.paratrip.member.validates.JoinMemberValidator;
 import paratrip.paratrip.member.validates.VerifyEmailMemberValidator;
 import paratrip.paratrip.member.validates.VerifyPasswordMemberValidator;
 import paratrip.paratrip.member.validates.VerifyUserIdMemberValidator;
@@ -30,6 +31,7 @@ public class MemberController {
 	private final VerifyEmailMemberValidator verifyEmailMemberValidator;
 	private final VerifyPasswordMemberValidator verifyPasswordMemberValidator;
 	private final VerifyUserIdMemberValidator verifyUserIdMemberValidator;
+	private final JoinMemberValidator joinMemberValidator;
 
 	private final MemberService memberService;
 
@@ -136,6 +138,50 @@ public class MemberController {
 		verifyUserIdMemberValidator.validate(request);
 
 		memberService.verifyMemberUserId(request.toVerifyUserIdMemberRequestDto());
+
+		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), "SUCCESS"));
+	}
+
+	@PostMapping(name = "회원 가입 (이전 Verify URL 전부 통과 이후 회원 가입 URL 요청 (EMAIL, USERID 중복 처리 똑같이 진행.))")
+	@Operation(summary = "회원 가입 요청 API", description = "회원 가입 요청 API (Gender의 경우 MALE, FEMAIL로 구분됩니다.)")
+	@ApiResponses(value = {
+		@ApiResponse(
+			responseCode = "200",
+			description = "요청에 성공하였습니다.",
+			useReturnTypeSchema = true),
+		@ApiResponse(
+			responseCode = "S500",
+			description = "500 SERVER_ERROR (나도 몰라 ..)",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(
+			responseCode = "B001",
+			description = "400 Invalid DTO Parameter errors / 요청 값 형식 요류",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(
+			responseCode = "UIDC002",
+			description = "USER_ID_DUPLICATION_CONFLICT_EXCEPTION / userId 중복 요류",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(
+			responseCode = "EDC001",
+			description = "409 EMAIL_DUPLICATION_CONFLICT_EXCEPTION / 이메일 중복 요류",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+	})
+	public ResponseEntity<BaseResponse> joinMember(
+		@Valid
+		@RequestBody JoinMemberRequest request
+	) {
+		// 유효성 검사
+		joinMemberValidator.validate(request);
+
+		memberService.joinMember(request.toJoinMemberRequestDto());
 
 		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), "SUCCESS"));
 	}
