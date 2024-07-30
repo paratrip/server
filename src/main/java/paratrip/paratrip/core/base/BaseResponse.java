889cfb91b81e93e08492f553e47ef0c71639e724
euics <pjh575157@gmail.com>
2024-07-30 15:59:56 +0900

    feat: BaseResponse 클래스 추가 (#1)
    - BaseResponse 클래스 정의: API 응답을 표준화하기 위한 기본 응답 클래스
    - status, message, data 필드 추가: 응답 상태 코드, 메시지, 데이터 포함
    - ofSuccess 메서드 추가: 성공 응답 생성 메서드
      - status와 data를 받아 "SUCCESS" 메시지와 함께 BaseResponse 객체 반환
    - ofFail 메서드 추가: 실패 응답 생성 메서드
      - status와 message를 받아 데이터 없이 BaseResponse 객체 반환

---
+++ src/main/java/paratrip/paratrip/core/base/BaseResponse.java
@@ -0,0 +1,23 @@
+package paratrip.paratrip.core.base;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor(staticName = "of")
+final public class BaseResponse<T> {
+
+	private final int status;
+	private final String message;
+	private final T data;
+
+	public static <T> BaseResponse<T> ofSuccess(int status, T data) {
+		return new BaseResponse<>(status, "SUCCESS", data);
+	}
+
+	public static <T> BaseResponse<T> ofFail(int status, String message) {
+		return new BaseResponse<>(status, message, null);
+	}
+}
