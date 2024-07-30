e61743fb75b73b8b7ac9dbdd0760011b6b157b29
euics <pjh575157@gmail.com>
2024-07-30 16:13:09 +0900

    feat: 비밀번호 유효성 검사 API 추가 (#1)
    - 비밀번호 유효성 검사 엔드포인트 추가
      - @PostMapping(value = "verify-password", name = "비밀번호 유효성 검사 (형식: 영문 + 숫자 + 특수문자 포함 8자리)") 어노테이션 사용하여 엔드포인트 설정
      - @Operation 및 @ApiResponses 어노테이션 사용하여 API 문서화 및 응답 코드 설명
        - 200: 요청 성공
        - 500: 서버 오류
        - 400: 요청 값 형식 오류
      - 비밀번호 유효성 검사 로직 구현
        - verifyPasswordMemberValidator.validate(request) 호출하여 요청 값 유효성 검사
        - 성공 응답 반환: BaseResponse.ofSuccess(HttpStatus.OK.value(), "SUCCESS")

--- src/main/java/paratrip/paratrip/member/controller/MemberController.java
+++ src/main/java/paratrip/paratrip/member/controller/MemberController.java
@@ -20,17 +20,19 @@ import paratrip.paratrip.core.base.BaseResponse;
 import paratrip.paratrip.core.exception.GlobalExceptionHandler;
 import paratrip.paratrip.member.service.MemberService;
 import paratrip.paratrip.member.validates.VerifyEmailMemberValidator;
+import paratrip.paratrip.member.validates.VerifyPasswordMemberValidator;
 
 @RestController
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
 	private final VerifyEmailMemberValidator verifyEmailMemberValidator;
+	private final VerifyPasswordMemberValidator verifyPasswordMemberValidator;
 
 	private final MemberService memberService;
 
-	@PostMapping(value = "verify-email", name = "이메일 인증 (중복 확인)")
-	@Operation(summary = "이메일 인증 (중복 확인) API", description = "이메일 인증 (중복 확인) API")
+	@PostMapping(value = "verify-email", name = "이메일 인증 (중복 확인 + 이메일 형식 검사)")
+	@Operation(summary = "이메일 인증 (중복 확인 + 이메일 형식 검사) API", description = "이메일 인증 (중복 확인 + 이메일 형식 검사) API")
 	@ApiResponses(value = {
 		@ApiResponse(
 			responseCode = "200",
@@ -64,6 +66,36 @@ public class MemberController {
 
 		// VO -> DTO
 		memberService.verifyMemberEmail(request.toVerifyEmailMemberRequestDto());
+
+		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), "SUCCESS"));
+	}
+
+	@PostMapping(value = "verify-password", name = "비밀번호 유효성 검사 (형식: 영문 + 숫자 + 특수문자 포함 8자리)")
+	@Operation(summary = "비밀번호 유효성 검사 (형식: 영문 + 숫자 + 특수문자 포함 8자리) API", description = "비밀번호 유효성 검사 (형식: 영문 + 숫자 + 특수문자 포함 8자리) API")
+	@ApiResponses(value = {
+		@ApiResponse(
+			responseCode = "200",
+			description = "요청에 성공하였습니다.",
+			useReturnTypeSchema = true),
+		@ApiResponse(
+			responseCode = "S500",
+			description = "500 SERVER_ERROR (나도 몰라 ..)",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(
+			responseCode = "B001",
+			description = "400 Invalid DTO Parameter errors / 요청 값 형식 요류",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+	})
+	public ResponseEntity<BaseResponse> addHideFeed(
+		@Valid
+		@RequestBody VerifyPasswordMemberRequest request
+	) {
+		// 유효성 검사
+		verifyPasswordMemberValidator.validate(request);
 
 		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), "SUCCESS"));
 	}
