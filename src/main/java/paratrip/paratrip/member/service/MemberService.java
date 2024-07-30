d8cc9b0bd03d190a2e258b4c58604a13c55d564a
euics <pjh575157@gmail.com>
2024-07-30 17:08:12 +0900

    feat: 회원 가입 서비스 로직 추가 (#1)
    - 회원 가입 처리 메서드 joinMember 추가
      - 이메일 중복 검사
      - 사용자 ID 중복 검사
      - 회원 정보를 MemberEntity로 변환하여 데이터베이스에 저장
      - 비밀번호는 BCryptPasswordEncoder를 사용하여 암호화

--- src/main/java/paratrip/paratrip/member/service/MemberService.java
+++ src/main/java/paratrip/paratrip/member/service/MemberService.java
@@ -2,10 +2,12 @@ package paratrip.paratrip.member.service;
 
 import static paratrip.paratrip.member.dto.request.MemberRequestDto.*;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import paratrip.paratrip.member.mapper.MemberMapper;
 import paratrip.paratrip.member.repository.MemberRepository;
 
 @Service
@@ -13,10 +15,13 @@ import paratrip.paratrip.member.repository.MemberRepository;
 public class MemberService {
 	private final MemberRepository memberRepository;
 
+	private final MemberMapper memberMapper;
+	private final BCryptPasswordEncoder encoder;
+
 	@Transactional(readOnly = true)
 	public void verifyMemberEmail(VerifyEmailMemberRequestDto verifyEmailMemberRequestDto) {
 		/*
-		 1. Email 유효성 검사
+		 1. Email 중복 검사
 		*/
 		memberRepository.isDuplicatedEmail(verifyEmailMemberRequestDto.email());
 	}
@@ -24,8 +29,24 @@ public class MemberService {
 	@Transactional(readOnly = true)
 	public void verifyMemberUserId(VerifyUserIdMemberRequestDto verifyUserIdMemberRequest) {
 		/*
-		 1. UserId 유효성 검사
+		 1. UserId 중복 검사
 		*/
 		memberRepository.isDuplicatedUserId(verifyUserIdMemberRequest.userId());
+	}
+
+	@Transactional
+	public void joinMember(JoinMemberRequestDto joinMemberRequestDto) {
+		/*
+		 1. Email 중복 검사
+		 2. UserId 중복 검사
+		*/
+		memberRepository.isDuplicatedEmail(joinMemberRequestDto.email());
+		memberRepository.isDuplicatedUserId(joinMemberRequestDto.userId());
+
+		// Member 저장
+		memberRepository.saveMemberEntity(memberMapper.toMemberEntity(
+			joinMemberRequestDto,
+			encoder.encode(joinMemberRequestDto.password()))
+		);
 	}
 }
