b5d0fc1e4ed28582fba3389ea1875ba43691c018
euics <pjh575157@gmail.com>
2024-07-30 16:01:48 +0900

    feat: MemberEntity 클래스 및 Gender 열거형 추가 (#1)
    - MemberEntity 클래스 정의: 회원 정보를 저장하는 엔티티
      - @Entity 및 @Table 어노테이션을 사용하여 MEMBER 테이블에 매핑
      - @Id 및 @GeneratedValue 어노테이션을 사용하여 memberSeq 필드를 기본 키로 설정
      - 이메일, 비밀번호, 전화번호, 사용자 ID, 생년월일, 성별 필드 정의
      - @Enumerated(EnumType.STRING)을 사용하여 성별 필드를 문자열로 매핑
      - Lombok 어노테이션(@AllArgsConstructor, @NoArgsConstructor, @Getter, @Builder)을 사용하여 코드 간결화
      - @DynamicInsert와 @DynamicUpdate 어노테이션을 사용하여 변경된 필드만 삽입 또는 업데이트
    
    - Gender 열거형 추가: 회원의 성별을 나타내는 열거형
      - MALE과 FEMALE 값을 가짐

---
+++ src/main/java/paratrip/paratrip/member/entity/MemberEntity.java
@@ -0,0 +1,52 @@
+package paratrip.paratrip.member.entity;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import paratrip.paratrip.member.util.Gender;
+
+@Entity
+@Table(name = "MEMBER")
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamicInsert
+@DynamicUpdate
+@Getter
+@Builder(toBuilder = true)
+public class MemberEntity {
+	@Id
+	@Column(name = "member_seq")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long memberSeq;
+
+	@Column(name = "email", nullable = false)
+	private String email;
+
+	@Column(name = "password", nullable = false)
+	private String encodedPassword;
+
+	@Column(name = "phone_number", nullable = false)
+	private String phoneNumber;
+
+	@Column(name = "user_id", nullable = false)
+	private String userId;
+
+	@Column(name = "birth", nullable = false)
+	private String birth;
+
+	@Column(name = "gender", nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Gender gender;
+}
