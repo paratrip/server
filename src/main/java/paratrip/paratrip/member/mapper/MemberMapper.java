0bdd0a2f98de23bf78afd13416fb89cb98615f90
euics <pjh575157@gmail.com>
2024-07-30 17:08:49 +0900

    feat: 회원 정보 매핑을 위한 MemberMapper 인터페이스 추가 (#1)
    - MapStruct를 이용한 MemberMapper 인터페이스 추가
    - JoinMemberRequestDto를 MemberEntity로 매핑하는 메서드 toMemberEntity 정의
      - memberSeq 필드는 무시
      - 비밀번호는 인코딩된 형태로 매핑

--- src/main/java/paratrip/paratrip/member/mapper/MemberMapper.java
+++ src/main/java/paratrip/paratrip/member/mapper/MemberMapper.java
@@ -1,11 +1,17 @@
 package paratrip.paratrip.member.mapper;
 
+import static paratrip.paratrip.member.dto.request.MemberRequestDto.*;
+
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
+import paratrip.paratrip.member.entity.MemberEntity;
+
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR, unmappedTargetPolicy = ReportingPolicy.IGNORE)
-
 public interface MemberMapper {
-
+	@Mapping(target = "memberSeq", ignore = true)
+	@Mapping(target = "encodedPassword", source = "encodedPassword")
+	MemberEntity toMemberEntity(JoinMemberRequestDto request, String encodedPassword);
 }
