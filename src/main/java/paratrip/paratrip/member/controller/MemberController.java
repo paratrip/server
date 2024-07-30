f0c0ab5ac08933ca01362b169ae83bd9364e46a6
euics <pjh575157@gmail.com>
2024-07-30 16:04:50 +0900

    feat: MemberController 클래스 추가 및 이메일 인증 API 구현 (#1)
    - MemberController 클래스 정의: 이메일 인증(중복 확인) 기능을 위한 REST API 엔드포인트 제공
      - @RestController 및 @RequestMapping("/member") 어노테이션 사용하여 컨트롤러 설정
      - @RequiredArgsConstructor 어노테이션 사용하여 의존성 주입
      - verifyEmailMemberValidator 및 memberService 의존성 주입
    - 이메일 인증(중복 확인) API 추가
      - @PostMapping(value = "email-verify", name = "이메일 인증 (중복 확인)") 어노테이션 사용하여 엔드포인트 설정
      - @Operation 및 @ApiResponses 어노테이션 사용하여 API 문서화 및 응답 코드 설명
        - 200: 요청 성공
        - 500: 서버 오류
        - 400: 요청 값 형식 오류
        - 409: 이메일 중복 오류
      - addHideFeed 메서드 추가: 이메일 인증(중복 확인) 로직 구현
        - verifyEmailMemberValidator.validate(request) 호출하여 요청 값 유효성 검사
        - memberService.verifyMemberEmail(request.toVerifyEmailMemberRequestDto()) 호출하여 이메일 중복 여부 확인
        - 성공 응답 반환: BaseResponse.ofSuccess(HttpStatus.OK.value(), "SUCCESS")

---
+++ src/main/java/paratrip/paratrip/member/controller/MemberController.java
@@ -0,0 +1,70 @@
+package paratrip.paratrip.member.controller;
+
+import static paratrip.paratrip.member.vo.request.MemberRequestVo.*;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import paratrip.paratrip.core.base.BaseResponse;
+import paratrip.paratrip.core.exception.GlobalExceptionHandler;
+import paratrip.paratrip.member.service.MemberService;
+import paratrip.paratrip.member.validates.VerifyEmailMemberValidator;
+
+@RestController
+@RequestMapping("/member")
+@RequiredArgsConstructor
+public class MemberController {
+	private final VerifyEmailMemberValidator verifyEmailMemberValidator;
+
+	private final MemberService memberService;
+
+	@PostMapping(value = "verify-email", name = "이메일 인증 (중복 확인)")
+	@Operation(summary = "이메일 인증 (중복 확인) API", description = "이메일 인증 (중복 확인) API")
+	@ApiResponses(value = {
+		@ApiResponse(
+			responseCode = "200",
+			description = "요청에 성공하였습니다.",
+			useReturnTypeSchema = true),
+		@ApiResponse(
+			responseCode = "S500",
+			description = "500 SERVER_ERROR (나도 몰라 ..)",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(
+			responseCode = "B001",
+			description = "400 Invalid DTO Parameter errors / 요청 값 형식 요류",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(
+			responseCode = "EDC001",
+			description = "409 EMAIL_DUPLICATION_CONFLICT_EXCEPTION / 이메일 중복 요류",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+	})
+	public ResponseEntity<BaseResponse> addHideFeed(
+		@Valid
+		@RequestBody VerifyEmailMemberRequest request
+	) {
+		// 유효성 검사
+		verifyEmailMemberValidator.validate(request);
+
+		// VO -> DTO
+		memberService.verifyMemberEmail(request.toVerifyEmailMemberRequestDto());
+
+		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), "SUCCESS"));
+	}
+}
