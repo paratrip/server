3bc1fab4fe3374b43635812f169613973673cf00
euics <pjh575157@gmail.com>
2024-08-02 20:57:26 +0900

    feat: JwtTokenFilter 빈 설정 수정 (#23)
    - JwtTokenFilter 빈 설정 메서드 수정
      - JWT 토큰 검사가 필요 없는 경로 목록(permitAllEndpoints) 정의
      - 블랙리스트용 RedisTemplate을 주입받아 JwtTokenFilter 생성

--- src/main/java/paratrip/paratrip/core/configuration/SecurityConfiguration.java
+++ src/main/java/paratrip/paratrip/core/configuration/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -16,59 +17,70 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import lombok.RequiredArgsConstructor;
 import paratrip.paratrip.core.filter.JwtTokenFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
-    @Value("${jwt.secret}")
-    private String jwtSecret;
+	private final RedisTemplate<String, String> blackListTemplate;
+	@Value("${jwt.secret}")
+	private String jwtSecret;
 
-    @Bean
-    @ConditionalOnProperty(name = "spring.h2.console.enabled", havingValue = "true")
-    public WebSecurityCustomizer configureH2ConsoleEnable() {
-        return web -> web.ignoring()
-                .requestMatchers(PathRequest.toH2Console());
-    }
+	@Bean
+	@ConditionalOnProperty(name = "spring.h2.console.enabled", havingValue = "true")
+	public WebSecurityCustomizer configureH2ConsoleEnable() {
+		return web -> web.ignoring()
+			.requestMatchers(PathRequest.toH2Console());
+	}
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/**").permitAll()
-                        .anyRequest().authenticated())
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(authorize -> authorize
+				.requestMatchers("/**").permitAll()
+				.anyRequest().authenticated())
+			.sessionManagement(session ->
+				session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        return http.build();
-    }
+		return http.build();
+	}
 
-    @Bean
-    public BCryptPasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	public BCryptPasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    @Bean
-    public JwtTokenFilter jwtTokenFilter() {
-        List<String> permitAllEndpoints = Arrays.asList(
-                // 토큰 검사가 필요 없는 경로 목록
-                // Swagger
-                "/swagger-ui/**",
-                "/v3/api-docs/**",
+	@Bean
+	public JwtTokenFilter jwtTokenFilter() {
+		List<String> permitAllEndpoints = Arrays.asList(
+			// 토큰 검사가 필요 없는 경로 목록
+			// Swagger
+			"/swagger-ui/**",
+			"/v3/api-docs/**",
 
-                // Health Check
-                "/actuator/health",
+			// Health Check
+			"/actuator/health",
 
-                // H2-Console
-                "/h2-console/**",
+			// H2-Console
+			"/h2-console/**",
 
-                // 모든 URL 개방
-                "/**"
-        );
-        return new JwtTokenFilter(jwtSecret, permitAllEndpoints);
-    }
+			// 개방 URL
+			"/sms-certification/**",
+			"/member/verify-userId",
+			"/member/verify-phone",
+			"/member/verify-password",
+			"/member/verify-email",
+			"/member/reset-password",
+			"/member/join",
+			"/member/login",
+			"/member/find-email"
+		);
+		return new JwtTokenFilter(jwtSecret, permitAllEndpoints, blackListTemplate);
+	}
 
 }
