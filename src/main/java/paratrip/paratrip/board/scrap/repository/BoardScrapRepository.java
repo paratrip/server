ce54a601184d04c205359be760039dd36ffd8932
euics <pjh575157@gmail.com>
2024-08-04 20:59:47 +0900

    feat: 게시물 스크랩 저장 메서드 추가 (#32)
    - BoardScrapRepository 인터페이스에 saveBoardScrapEntity 메서드 정의
      - BoardScrapEntity를 저장하는 메서드
    - BoardScrapRepositoryImpl 클래스에 saveBoardScrapEntity 메서드 구현
      - boardScrapJpaRepository를 사용하여 BoardScrapEntity를 저장
      - 저장된 BoardScrapEntity를 반환

--- src/main/java/paratrip/paratrip/board/scrap/repository/BoardScrapRepository.java
+++ src/main/java/paratrip/paratrip/board/scrap/repository/BoardScrapRepository.java
@@ -1,4 +1,7 @@
 package paratrip.paratrip.board.scrap.repository;
 
+import paratrip.paratrip.board.scrap.entity.BoardScrapEntity;
+
 public interface BoardScrapRepository {
+	BoardScrapEntity saveBoardScrapEntity(BoardScrapEntity boardScrapEntity);
 }
--- src/main/java/paratrip/paratrip/board/scrap/repository/BoardScrapRepositoryImpl.java
+++ src/main/java/paratrip/paratrip/board/scrap/repository/BoardScrapRepositoryImpl.java
@@ -3,8 +3,15 @@ package paratrip.paratrip.board.scrap.repository;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
+import paratrip.paratrip.board.scrap.entity.BoardScrapEntity;
 
 @Component
 @RequiredArgsConstructor
 public class BoardScrapRepositoryImpl implements BoardScrapRepository {
+	private final BoardScrapJpaRepository boardScrapJpaRepository;
+
+	@Override
+	public BoardScrapEntity saveBoardScrapEntity(BoardScrapEntity boardScrapEntity) {
+		return boardScrapJpaRepository.save(boardScrapEntity);
+	}
 }
