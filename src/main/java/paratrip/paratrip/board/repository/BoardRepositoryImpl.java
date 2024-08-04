ae61156d44286d733bd14565b721947c64f4ea8a
euics <pjh575157@gmail.com>
2024-08-04 15:59:48 +0900

    feat: 게시물 조회 메서드 추가 및 예외 처리 (#29)
    - BoardRepository 인터페이스에 게시물 조회 메서드 정의
      - findByBoardSeq(Long boardSeq): 게시물 번호로 게시물 조회
      - findByMemberEntity(MemberEntity memberEntity): 회원 엔티티로 게시물 조회
    - BoardRepositoryImpl 클래스에 게시물 조회 메서드 구현
      - findByBoardSeq 메서드: 게시물 번호로 게시물 조회, 존재하지 않을 경우 BOARD_SEQ_BAD_REQUEST_EXCEPTION 발생
      - findByMemberEntity 메서드: 회원 엔티티로 게시물 조회, 존재하지 않을 경우 BOARD_NOT_CREATED_BY_MEMBER_BAD_REQUEST_EXCEPTION 발생

--- src/main/java/paratrip/paratrip/board/repository/BoardRepositoryImpl.java
+++ src/main/java/paratrip/paratrip/board/repository/BoardRepositoryImpl.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
 import paratrip.paratrip.board.entity.BoardEntity;
+import paratrip.paratrip.core.exception.BadRequestException;
+import paratrip.paratrip.core.exception.ErrorResult;
+import paratrip.paratrip.member.entity.MemberEntity;
 
 @Component
 @RequiredArgsConstructor
@@ -13,5 +16,17 @@ public class BoardRepositoryImpl implements BoardRepository {
 	@Override
 	public BoardEntity saveBoardEntity(BoardEntity boardEntity) {
 		return boardJpaRepository.save(boardEntity);
+	}
+
+	@Override
+	public BoardEntity findByBoardSeq(Long boardSeq) {
+		return boardJpaRepository.findByBoardSeq(boardSeq)
+			.orElseThrow(() -> new BadRequestException(ErrorResult.BOARD_SEQ_BAD_REQUEST_EXCEPTION));
+	}
+
+	@Override
+	public BoardEntity findByMemberEntity(MemberEntity memberEntity) {
+		return boardJpaRepository.findByMemberEntity(memberEntity)
+			.orElseThrow(() -> new BadRequestException(ErrorResult.BOARD_NOT_CREATED_BY_MEMBER_BAD_REQUEST_EXCEPTION));
 	}
 }
