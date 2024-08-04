62f2ae7d9afa3722d77c40615272c9fe39c829a8
euics <pjh575157@gmail.com>
2024-08-04 21:02:20 +0900

    feat: 게시물 스크랩 서비스 로직 추가 (#32)
    - BoardScrapService 클래스 추가
      - AddBoardScrapRequestDto를 받아 게시물 스크랩을 저장하는 로직 추가
      - Member 유효성 검사
      - Board 유효성 검사
      - 작성자 유효성 검사
      - BoardScrapEntity 저장 후 AddBoardScrapResponseDto 반환

---
+++ src/main/java/paratrip/paratrip/board/scrap/service/BoardScrapService.java
@@ -0,0 +1,46 @@
+package paratrip.paratrip.board.scrap.service;
+
+import static paratrip.paratrip.board.scrap.service.dto.request.BoardScarpRequestDto.*;
+import static paratrip.paratrip.board.scrap.service.dto.response.BoardScrapResponseDto.*;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import paratrip.paratrip.board.main.entity.BoardEntity;
+import paratrip.paratrip.board.main.repository.BoardRepository;
+import paratrip.paratrip.board.scrap.entity.BoardScrapEntity;
+import paratrip.paratrip.board.scrap.mapper.BoardScrapMapper;
+import paratrip.paratrip.board.scrap.repository.BoardScrapRepository;
+import paratrip.paratrip.member.entity.MemberEntity;
+import paratrip.paratrip.member.repository.MemberRepository;
+
+@Service
+@RequiredArgsConstructor
+public class BoardScrapService {
+	private final MemberRepository memberRepository;
+	private final BoardRepository boardRepository;
+	private final BoardScrapRepository boardScrapRepository;
+
+	private final BoardScrapMapper boardScrapMapper;
+
+	@Transactional
+	public AddBoardScrapResponseDto saveBoardScrap(AddBoardScrapRequestDto addBoardScrapRequestDto) {
+		/*
+		 1. Member 유효성 검사
+		 2. Board 유효성 검사
+		 3. 작성자 유효성 검사
+		*/
+		MemberEntity memberEntity = memberRepository.findByMemberSeq(addBoardScrapRequestDto.memberSeq());
+		boardRepository.findByBoardSeq(addBoardScrapRequestDto.boardSeq());
+		BoardEntity boardEntity
+			= boardRepository.findByCreatorMemberEntityAndBoardSeq(memberEntity, addBoardScrapRequestDto.boardSeq());
+
+		// 저장
+		BoardScrapEntity boardScrapEntity = boardScrapRepository.saveBoardScrapEntity(
+			boardScrapMapper.toBoardScrapEntity(memberEntity, boardEntity)
+		);
+
+		return new AddBoardScrapResponseDto(boardScrapEntity.getBoardScrapSeq());
+	}
+}
