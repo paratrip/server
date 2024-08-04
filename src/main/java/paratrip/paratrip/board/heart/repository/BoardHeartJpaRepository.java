a6f5afcb3e85b871291e779899d94ddd57776778
euics <pjh575157@gmail.com>
2024-08-04 17:42:05 +0900

    feat: 게시물 좋아요 조회 및 삭제 메서드 추가 (#32)
    - BoardHeartRepository 인터페이스에 게시물 좋아요 관련 메서드 정의
      - findByBoardHeartSeq(Long boardHeartSeq): 게시물 좋아요 번호로 게시물 좋아요 엔티티 조회
      - findByBoardHeartSeqAndMemberEntity(Long boardHeartSeq, MemberEntity memberEntity): 게시물 좋아요 번호와 회원 엔티티로 게시물 좋아요 엔티티 조회
      - deleteBoardHeartEntity(BoardHeartEntity boardHeartEntity): 게시물 좋아요 엔티티 삭제
    
    - BoardHeartRepositoryImpl 클래스에 게시물 좋아요 관련 메서드 구현
      - findByBoardHeartSeq 메서드: 게시물 좋아요 번호로 게시물 좋아요 엔티티 조회, 존재하지 않을 경우 BOARD_HEART_SEQ_BAD_REQUEST_EXCEPTION 발생
      - findByBoardHeartSeqAndMemberEntity 메서드: 게시물 좋아요 번호와 회원 엔티티로 게시물 좋아요 엔티티 조회, 존재하지 않을 경우 BOARD_HEART_NOT_FOUND_EXCEPTION 발생
      - deleteBoardHeartEntity 메서드: 게시물 좋아요 엔티티 삭제

--- src/main/java/paratrip/paratrip/board/heart/repository/BoardHeartJpaRepository.java
+++ src/main/java/paratrip/paratrip/board/heart/repository/BoardHeartJpaRepository.java
@@ -1,11 +1,16 @@
 package paratrip.paratrip.board.heart.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import paratrip.paratrip.board.heart.entity.BoardHeartEntity;
+import paratrip.paratrip.member.entity.MemberEntity;
 
 @Repository
 public interface BoardHeartJpaRepository extends JpaRepository<BoardHeartEntity, Long> {
+	Optional<BoardHeartEntity> findByBoardHeartSeq(Long boardHeartSeq);
 
+	Optional<BoardHeartEntity> findByBoardHeartSeqAndMemberEntity(Long boardHeartSeq, MemberEntity memberEntity);
 }
--- src/main/java/paratrip/paratrip/board/heart/repository/BoardHeartRepositoryImpl.java
+++ src/main/java/paratrip/paratrip/board/heart/repository/BoardHeartRepositoryImpl.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
 import paratrip.paratrip.board.heart.entity.BoardHeartEntity;
+import paratrip.paratrip.core.exception.BadRequestException;
+import paratrip.paratrip.core.exception.ErrorResult;
+import paratrip.paratrip.member.entity.MemberEntity;
 
 @Component
 @RequiredArgsConstructor
@@ -13,5 +16,22 @@ public class BoardHeartRepositoryImpl implements BoardHeartRepository {
 	@Override
 	public BoardHeartEntity saveBoardHeartEntity(BoardHeartEntity boardHeartEntity) {
 		return boardHeartJpaRepository.save(boardHeartEntity);
+	}
+
+	@Override
+	public BoardHeartEntity findByBoardHeartSeq(Long boardHeartSeq) {
+		return boardHeartJpaRepository.findByBoardHeartSeq(boardHeartSeq)
+			.orElseThrow(() -> new BadRequestException(ErrorResult.BOARD_HEART_SEQ_BAD_REQUEST_EXCEPTION));
+	}
+
+	@Override
+	public BoardHeartEntity findByBoardHeartSeqAndMemberEntity(Long boardHeartSeq, MemberEntity memberEntity) {
+		return boardHeartJpaRepository.findByBoardHeartSeqAndMemberEntity(boardHeartSeq, memberEntity)
+			.orElseThrow(() -> new BadRequestException(ErrorResult.BOARD_HEART_NOT_FOUND_EXCEPTION));
+	}
+
+	@Override
+	public void deleteBoardHeartEntity(BoardHeartEntity boardHeartEntity) {
+		boardHeartJpaRepository.delete(boardHeartEntity);
 	}
 }
