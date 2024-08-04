16290a7b3efd8fc8d636004a8feb2c45c6938bed
euics <pjh575157@gmail.com>
2024-08-04 16:19:09 +0900

    feat: 게시물 스크랩 Entity/Repo 추가 (#32)
    - BoardScrapEntity 클래스 추가
      - @Entity 및 @Table(name = "BOARD_SCRAP") 어노테이션을 사용하여 데이터베이스 엔티티 매핑
      - boardScrapSeq 필드를 기본 키로 설정하고 자동 생성
      - MemberEntity와 BoardEntity와의 다대일 관계 설정 (Lazy Fetch 타입)
      - Lombok 어노테이션(@AllArgsConstructor, @NoArgsConstructor, @Getter, @Builder(toBuilder = true)) 사용하여 생성자 및 게터, 빌더 메서드 자동 생성
      - @DynamicInsert 및 @DynamicUpdate 어노테이션을 사용하여 변경된 필드만 업데이트
    
    - BoardScrapJpaRepository 인터페이스 추가
      - JpaRepository<BoardScrapEntity, Long>를 상속받아 기본 CRUD 메서드 제공
      - @Repository 어노테이션을 사용하여 스프링 데이터 레포지토리로 설정
    
    - BoardScrapRepository 인터페이스 추가
      - 스크랩 기능에 대한 커스텀 메서드를 정의할 인터페이스
    
    - BoardScrapRepositoryImpl 클래스 추가
      - BoardScrapRepository 인터페이스를 구현하는 빈 클래스
      - @Component 및 @RequiredArgsConstructor 어노테이션을 사용하여 스프링 빈으로 등록 및 생성자 주입 설정

---
+++ src/main/java/paratrip/paratrip/board/scrap/repository/BoardScrapRepositoryImpl.java
@@ -0,0 +1,10 @@
+package paratrip.paratrip.board.scrap.repository;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class BoardScrapRepositoryImpl implements BoardScrapRepository {
+}
