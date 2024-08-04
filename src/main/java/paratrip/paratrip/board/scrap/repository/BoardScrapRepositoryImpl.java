0f9073c6f8f3a958336c14e45e1bb10b38422e3a
euics <pjh575157@gmail.com>
2024-08-04 21:13:21 +0900

    feat: 게시물 스크랩 삭제 기능 구현 (#32)
    - 게시물 스크랩 엔티티 조회 메서드 추가
      - findByBoardScrapSeq(Long boardScrapSeq) 메서드 구현
      - findByMemberEntityAndBoardScrapSeq(MemberEntity memberEntity, Long boardScrapSeq) 메서드 구현
    - 게시물 스크랩 엔티티 삭제 메서드 추가
      - deleteBoardScrapEntity(BoardScrapEntity boardScrapEntity) 메서드 구현

--- src/main/java/paratrip/paratrip/board/scrap/repository/BoardScrapRepositoryImpl.java
+++ src/main/java/paratrip/paratrip/board/scrap/repository/BoardScrapRepositoryImpl.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
 import paratrip.paratrip.board.scrap.entity.BoardScrapEntity;
+import paratrip.paratrip.core.exception.BadRequestException;
+import paratrip.paratrip.core.exception.ErrorResult;
+import paratrip.paratrip.member.entity.MemberEntity;
 
 @Component
 @RequiredArgsConstructor
@@ -13,5 +16,22 @@ public class BoardScrapRepositoryImpl implements BoardScrapRepository {
 	@Override
 	public BoardScrapEntity saveBoardScrapEntity(BoardScrapEntity boardScrapEntity) {
 		return boardScrapJpaRepository.save(boardScrapEntity);
+	}
+
+	@Override
+	public BoardScrapEntity findByBoardScrapSeq(Long boardScrapSeq) {
+		return boardScrapJpaRepository.findByBoardScrapSeq(boardScrapSeq)
+			.orElseThrow(() -> new BadRequestException(ErrorResult.BOARD_SCRAP_SEQ_BAD_REQUEST_EXCEPTION));
+	}
+
+	@Override
+	public BoardScrapEntity findByMemberEntityAndBoardScrapSeq(MemberEntity memberEntity, Long boardScrapSeq) {
+		return boardScrapJpaRepository.findByMemberEntityAndBoardScrapSeq(memberEntity, boardScrapSeq)
+			.orElseThrow(() -> new BadRequestException(ErrorResult.BOARDS_SCRAP_NOT_FOUND_EXCEPTION));
+	}
+
+	@Override
+	public void deleteBoardScrapEntity(BoardScrapEntity boardScrapEntity) {
+		boardScrapJpaRepository.delete(boardScrapEntity);
 	}
 }
