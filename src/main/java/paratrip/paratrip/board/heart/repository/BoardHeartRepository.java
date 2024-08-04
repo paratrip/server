38788cbc5ff1004cbde6f5efdc3d519e8ced67c3
euics <pjh575157@gmail.com>
2024-08-04 16:39:36 +0900

    feat: 게시물 좋아요 저장 메서드 추가 (#32)
    - BoardHeartRepository 인터페이스에 saveBoardHeartEntity 메서드 정의
      - BoardHeartEntity를 저장하는 메서드
    - BoardHeartRepositoryImpl 클래스에 saveBoardHeartEntity 메서드 구현
      - boardHeartJpaRepository를 사용하여 BoardHeartEntity를 저장
      - 저장된 BoardHeartEntity를 반환

--- src/main/java/paratrip/paratrip/board/heart/repository/BoardHeartRepository.java
+++ src/main/java/paratrip/paratrip/board/heart/repository/BoardHeartRepository.java
@@ -1,4 +1,7 @@
 package paratrip.paratrip.board.heart.repository;
 
+import paratrip.paratrip.board.heart.entity.BoardHeartEntity;
+
 public interface BoardHeartRepository {
+	BoardHeartEntity saveBoardHeartEntity(BoardHeartEntity boardHeartEntity);
 }
--- src/main/java/paratrip/paratrip/board/heart/repository/BoardHeartRepositoryImpl.java
+++ src/main/java/paratrip/paratrip/board/heart/repository/BoardHeartRepositoryImpl.java
@@ -3,8 +3,15 @@ package paratrip.paratrip.board.heart.repository;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
+import paratrip.paratrip.board.heart.entity.BoardHeartEntity;
 
 @Component
 @RequiredArgsConstructor
 public class BoardHeartRepositoryImpl implements BoardHeartRepository {
+	private final BoardHeartJpaRepository boardHeartJpaRepository;
+
+	@Override
+	public BoardHeartEntity saveBoardHeartEntity(BoardHeartEntity boardHeartEntity) {
+		return boardHeartJpaRepository.save(boardHeartEntity);
+	}
 }
