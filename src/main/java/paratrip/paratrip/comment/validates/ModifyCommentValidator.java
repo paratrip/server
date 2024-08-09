2bc43c2bf8692922be77c2be34c21712adb185d4
euics <pjh575157@gmail.com>
2024-08-10 01:05:54 +0900

    feat: 댓글 수정 요청 유효성 검사 클래스 추가 (#46)
    - `ModifyCommentValidator` 클래스 추가.
      - `ModifyCommentRequest`의 `memberSeq` 및 `commentSeq` 필드에 대한 유효성 검사 메서드 추가.
      - 필드가 null인 경우 `BadRequestException` 발생.

---
+++ src/main/java/paratrip/paratrip/comment/validates/ModifyCommentValidator.java
@@ -0,0 +1,28 @@
+package paratrip.paratrip.comment.validates;
+
+import static paratrip.paratrip.comment.controller.vo.request.CommentRequestVo.*;
+
+import org.springframework.stereotype.Component;
+
+import paratrip.paratrip.core.exception.BadRequestException;
+import paratrip.paratrip.core.exception.ErrorResult;
+
+@Component
+public class ModifyCommentValidator {
+	public void validate(ModifyCommentRequest request) {
+		validateMemberSeq(request.memberSeq());
+		validateCommentSeq(request.commentSeq());
+	}
+
+	private void validateMemberSeq(Long memberSeq) {
+		if (memberSeq == null) {
+			throw new BadRequestException(ErrorResult.DTO_BAD_REQUEST_EXCEPTION);
+		}
+	}
+
+	private void validateCommentSeq(Long commentSeq) {
+		if (commentSeq == null) {
+			throw new BadRequestException(ErrorResult.DTO_BAD_REQUEST_EXCEPTION);
+		}
+	}
+}
