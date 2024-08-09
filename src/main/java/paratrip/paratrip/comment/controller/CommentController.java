a944c6a97c50632c79bd500921a581742092183a
euics <pjh575157@gmail.com>
2024-08-10 00:37:51 +0900

    feat: 댓글 작성 API 구현 (#46)
    - `CommentController` 클래스에 댓글 작성 API 추가
      - `/comment` 경로에 POST 요청으로 댓글 작성 기능 제공
      - `AddCommentRequestValidator`를 통해 요청 값에 대한 유효성 검사 수행
      - `CommentService`를 통해 비즈니스 로직 처리 및 데이터 저장
      - 응답으로 `AddCommentResponseVo` 반환

---
+++ src/main/java/paratrip/paratrip/comment/controller/CommentController.java
@@ -0,0 +1,80 @@
+package paratrip.paratrip.comment.controller;
+
+import static paratrip.paratrip.comment.controller.vo.request.CommentRequestVo.*;
+import static paratrip.paratrip.comment.controller.vo.response.CommentResponseVo.*;
+import static paratrip.paratrip.comment.service.dto.response.CommentResponseDto.*;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import paratrip.paratrip.comment.controller.validates.AddCommentRequestValidator;
+import paratrip.paratrip.comment.service.CommentService;
+import paratrip.paratrip.core.base.BaseResponse;
+import paratrip.paratrip.core.exception.GlobalExceptionHandler;
+
+@RestController
+@RequestMapping("/comment")
+@RequiredArgsConstructor
+@Tag(name = "댓글 API", description = "담당자(박종훈)")
+public class CommentController {
+	private final AddCommentRequestValidator addCommentRequestValidator;
+
+	private final CommentService commentService;
+
+	@PostMapping(name = "댓글 작성")
+	@Operation(summary = "댓글 작성 API", description = "댓글 작성 API")
+	@ApiResponses(value = {
+		@ApiResponse(
+			responseCode = "200",
+			description = "요청에 성공하였습니다.",
+			useReturnTypeSchema = true),
+		@ApiResponse(
+			responseCode = "S500",
+			description = "500 SERVER_ERROR (나도 몰라 ..)",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(
+			responseCode = "B001",
+			description = "400 Invalid DTO Parameter errors / 요청 값 형식 요류",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(
+			responseCode = "MSB003",
+			description = "400 MEMBER_SEQ_BAD_REQUEST_EXCEPTION / Member Seq 요류",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(
+			responseCode = "BSB005",
+			description = "400 BOARD_SEQ_BAD_REQUEST_EXCEPTION / Board Seq 요류",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+	})
+	public ResponseEntity<BaseResponse> addComment(
+		@Valid
+		@RequestBody AddCommentRequest request) {
+		// 유효성 검사
+		addCommentRequestValidator.validate(request);
+
+		// VO -> DTO
+		AddCommentResponseDto addCommentResponseDto = commentService.addComment(request.toAddCommentRequestDto());
+		AddCommentResponseVo response = addCommentResponseDto.toAddCommentResponseVo();
+
+		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), response));
+	}
+}
