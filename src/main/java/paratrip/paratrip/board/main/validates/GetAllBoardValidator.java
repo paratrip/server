acd737a2e0c013a307a56ec0f6725f5b878d4520
euics <pjh575157@gmail.com>
2024-08-10 02:31:51 +0900

    feat: 전체 게시물 조회 시 MemberSeq 유효성 검사 추가 (#46)
    - GetAllBoardValidator 추가
    - 조회 요청 시 MemberSeq의 유효성을 검사하여 Null 또는 유효하지 않은 값이 들어오는 것을 방지합니다.

---
+++ src/main/java/paratrip/paratrip/board/main/validates/GetAllBoardValidator.java
@@ -0,0 +1,19 @@
+package paratrip.paratrip.board.main.validates;
+
+import org.springframework.stereotype.Component;
+
+import paratrip.paratrip.core.exception.BadRequestException;
+import paratrip.paratrip.core.exception.ErrorResult;
+
+@Component
+public class GetAllBoardValidator {
+	public void validate(Long memberSeq) {
+		validateMemberSeq(memberSeq);
+	}
+
+	private void validateMemberSeq(Long memberSeq) {
+		if (memberSeq == null) {
+			throw new BadRequestException(ErrorResult.DTO_BAD_REQUEST_EXCEPTION);
+		}
+	}
+}
