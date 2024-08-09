587404529838a01eb8b98bc092e76f23ca82ade6
euics <pjh575157@gmail.com>
2024-08-10 02:28:13 +0900

    feat: 게시물 스크랩 중복 체크 및 카운트 기능 추가 (#32)
    - `countByBoardEntity` 메서드를 추가하여 특정 게시물에 대한 스크랩 개수를 조회할 수 있도록 구현.
    - `duplicateBoardScrap` 메서드를 통해 특정 회원이 이미 게시물을 스크랩했는지 중복 체크 기능 구현.
    - 중복된 스크랩이 존재할 경우 `ConflictException` 발생.

--- src/main/java/paratrip/paratrip/board/scrap/repository/BoardScrapJpaRepository.java
+++ src/main/java/paratrip/paratrip/board/scrap/repository/BoardScrapJpaRepository.java
@@ -5,11 +5,17 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import paratrip.paratrip.board.main.entity.BoardEntity;
 import paratrip.paratrip.board.scrap.entity.BoardScrapEntity;
 import paratrip.paratrip.member.entity.MemberEntity;
 
 @Repository
 public interface BoardScrapJpaRepository extends JpaRepository<BoardScrapEntity, Long> {
 	Optional<BoardScrapEntity> findByBoardScrapSeq(Long boardScrapSeq);
+
 	Optional<BoardScrapEntity> findByMemberEntityAndBoardScrapSeq(MemberEntity memberEntity, Long boardScrapSeq);
+
+	long countByBoardEntity(BoardEntity boardEntity);
+
+	Optional<BoardScrapEntity> findByMemberEntityAndBoardEntity(MemberEntity memberEntity, BoardEntity boardEntity);
 }
