8def1e0308ea1c3a173e456e195d7d9218144dc8
euics <pjh575157@gmail.com>
2024-08-13 17:37:39 +0900

    feat: 제목 기반 게시물 검색 구현 (#51)
    - `findByTitleContains` 메서드를 구현하여 `boardDocumentsElasticSearchRepository`를 통해 제목을 포함하는 게시물을 검색.
    - 검색 결과를 페이지네이션된 형태로 `Page<BoardDocuments>`로 반환하도록 설정.

--- src/main/java/paratrip/paratrip/board/search/repository/BoardDocumentsElasticSearchRepository.java
+++ src/main/java/paratrip/paratrip/board/search/repository/BoardDocumentsElasticSearchRepository.java
@@ -1,5 +1,7 @@
 package paratrip.paratrip.board.search.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import paratrip.paratrip.board.search.entity.BoardDocuments;
 
 @Repository
 public interface BoardDocumentsElasticSearchRepository extends ElasticsearchRepository<BoardDocuments, String> {
-
+	Page<BoardDocuments> findByTitleContains(String title, Pageable pageable);
 }
