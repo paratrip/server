92110c565693380990aee3ff9a98cb9075e52a39
euics <pjh575157@gmail.com>
2024-09-23 00:31:43 +0900

    feat: Change Alarm URL (#64)
    - / -> /alarm

--- src/main/java/paratrip/paratrip/alarm/controller/AlarmController.java
+++ src/main/java/paratrip/paratrip/alarm/controller/AlarmController.java
@@ -5,16 +5,20 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import paratrip.paratrip.alarm.service.AlarmService;
 import paratrip.paratrip.alarm.service.dto.response.AlarmResponseDto;
 import paratrip.paratrip.core.base.BaseResponse;
 
-@RestController(value = "/alarm")
+@RestController
+@RequestMapping("/alarm")
 @RequiredArgsConstructor
+@Tag(name = "알람 API")
 public class AlarmController {
 	private final AlarmService alarmService;
 
