8e00be83c6f9cb14541b6d15dd989f3f39544896
euics <pjh575157@gmail.com>
2024-09-24 04:19:05 +0900

    hotfix: Server 시간 오류 해결

--- src/main/java/paratrip/paratrip/core/configuration/JacksonConfig.java
+++ src/main/java/paratrip/paratrip/core/configuration/JacksonConfig.java
@@ -2,10 +2,10 @@ package paratrip.paratrip.core.configuration;
 
 import java.util.TimeZone;
 
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
@@ -16,6 +16,10 @@ public class JacksonConfig {
 		ObjectMapper objectMapper = new ObjectMapper();
 		objectMapper.registerModule(new JavaTimeModule());
 		objectMapper.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+
+		// LocalDateTime을 문자열로 직렬화하도록 설정
+		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
 		return objectMapper;
 	}
 }
