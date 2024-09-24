cb585a4b594e1a6eadaf9999a34288a840f78207
euics <pjh575157@gmail.com>
2024-09-24 22:03:44 +0900

    chore: Swagger 세팅

--- src/main/java/paratrip/paratrip/ParatripApplication.java
+++ src/main/java/paratrip/paratrip/ParatripApplication.java
@@ -5,9 +5,12 @@ import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "https://euics.co.kr", description = "관광데이터 공모전 SERVER URI")})
 public class ParatripApplication {
 
 	public static void main(String[] args) {
